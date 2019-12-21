9b2db4f3228f743256ff8de4f1bb07b82279d6ba
jepark-tech <58829123+jepark-tech@users.noreply.github.com>
2019-12-20 21:25:28 -0500

    Add EqualSumChecker class that has hasEqualSum method

---
+++ src/codingExercise/EqualSumChecker.java
@@ -0,0 +1,10 @@
+package codingExercise;
+
+public class EqualSumChecker {
+    public static boolean hasEqualSum (int a, int b, int c){
+        if(a + b == c){
+            return true;
+        }
+        return false;
+    }
+}
