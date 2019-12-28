4e0a0e006339ae9acd0d0ea7af77d3afe34c2173
jepark-tech <58829123+jepark-tech@users.noreply.github.com>
2019-12-27 23:13:37 -0500

    Add SimpleCalculator Class

---
+++ src/codingExercise/SimpleCalculator.java
@@ -0,0 +1,42 @@
+package codingExercise;
+
+public class SimpleCalculator {
+	private double firstNumber, secondNumber;
+	
+	public double getFirstNumber() {
+		return firstNumber;
+	}
+	
+	public double getSecondNumber() {
+		return secondNumber;
+	}
+	
+	public void setFirstNumber(double a) {
+		this.firstNumber = a;
+	}
+	
+	public void setSecondNumber(double b) {
+		this.secondNumber = b;
+	}
+	
+	public double getAdditionResult() {
+		return firstNumber + secondNumber;
+	}
+	
+	public double getSubtractionResult() {
+		return firstNumber - secondNumber;
+	}
+	
+	public double getMultiplicationResult() {
+		return firstNumber * secondNumber;
+	}
+	
+	public double getDivisionResult() {
+		if(secondNumber == 0) {
+			return 0;
+		}
+		return firstNumber / secondNumber;
+	}
+	
+	
+}
